46cbcc27d88b37abc5af278aad81efe99e6400f9
Arman <96230050+ArmanSel@users.noreply.github.com>
2023-03-05 15:38:08 +0100

    feat: Created view switch between MainActivity.kt and NewMeetingActivity.kt

--- app/src/main/java/ch/selimovic/scrumpro/MainActivity.kt
+++ app/src/main/java/ch/selimovic/scrumpro/MainActivity.kt
@@ -1,11 +1,20 @@
 package ch.selimovic.scrumpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCreate = findViewById<Button>(R.id.btnCreate)
+
+        btnCreate.setOnClickListener {
+            val intent = Intent(this, NewMeetingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ch/selimovic/scrumpro/NewMeetingActivity.kt
@@ -0,0 +1,12 @@
+package ch.selimovic.scrumpro
+
+import android.app.Activity
+import android.os.Bundle
+
+class NewMeetingActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newmeeting)
+        // add your initialization code here
+    }
+}
