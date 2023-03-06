f4e1a5a67d1d21eeafcab660c6dabb1d277f0a03
Arman <96230050+ArmanSel@users.noreply.github.com>
2023-03-06 13:17:14 +0100

    feat: Created a barebones page to edit/delete meetings

---
+++ app/src/main/java/ch/selimovic/scrumpro/ui/EditMeetingActivity.kt
@@ -0,0 +1,12 @@
+package ch.selimovic.scrumpro.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.selimovic.scrumpro.R
+
+class EditMeetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_meeting)
+    }
+}
